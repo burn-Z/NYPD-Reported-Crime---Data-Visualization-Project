1d67953fcc2f012c180e94b2ca00d4b6598d6724
Burnsley Lafleur <78240258+burn-Z@users.noreply.github.com>
2022-12-03 22:16:43 -0500

    new filters and charts

--- web-app/Home.py
+++ web-app/Home.py
@@ -14,7 +14,7 @@ st.set_page_config(
     initial_sidebar_state= "auto",
 )
 
-# # consitent pathing
+# # consistent pathing
 # cur_path = os.path.join(os.getcwd(), 'style.css')
 # print('------', cur_path)
 
@@ -35,37 +35,101 @@ def get_data_csv():
 
 df = get_data_csv()
 
-# getting filter fields
-available_boroughs = df.borough.unique()
-available_years = df.year.unique()
-available_category = df.category.unique()
+borough = 'borough'
+year = 'year'
+category = 'category'
+offense = 'offense'
 
-# UI for filters
-slct_boro = st.sidebar.multiselect('Filter by Borough', available_boroughs, available_boroughs)
+# getting filter fields
+available_boroughs = df[borough].unique()
+available_years = df[year].unique()
+available_ofns_desc = df[category].unique()
+
+# Sidebar filters
+slct_boro = st.sidebar.multiselect('Filter by Borough', available_boroughs, available_boroughs[1])
 st.sidebar.markdown('   ---')
 slct_yr = st.sidebar.multiselect('Filter by Year', available_years, available_years[-1])
-# crime_cats = st.sidebar.multiselect('Filter by Crime Category', available_category)
+st.sidebar.markdown('   ---')
+slct_ofnsc = st.sidebar.multiselect('Filter by Offence Category', available_ofns_desc, available_ofns_desc[-1])
 
 # Logic for filter selections
-if not (slct_boro or slct_yr):
-    df_slct = df
-elif not slct_boro and slct_yr:
-    df_slct = df.query("year == @slct_yr")
-elif slct_boro and not slct_yr:
-    df_slct = df.query("borough in @slct_boro")
+if not slct_boro:
+    slct_boro = available_boroughs.tolist()
+
+if not slct_yr:
+    slct_yr = available_years.tolist()
+
+if not slct_ofnsc:
+    slct_ofnsc = available_ofns_desc.tolist()
+
+
+# query data based on selection
+df_slct = df.query(f"{borough} in @slct_boro & {year} in @slct_yr & {category} in @slct_ofnsc")
+
+
+####
+slct_yr.sort()
+
+if len(slct_yr) == 1:
+    # current year crime count
+    temp0 = df_slct.shape[0]
+
+    # previous year crime count
+    prev_y = slct_yr[0] - 1
+    temp1 = df.query(f'{year} == @prev_y & {borough} in @slct_boro & {category} in @slct_ofnsc').shape[0]
 else:
-    df_slct = df.query("borough in @slct_boro & year in @slct_yr")
+    # current year crime count
+    sy = slct_yr[-1]
+    temp0 = df_slct.query(f'{year} == @sy').shape[0]
+
+    #...
+    sy = slct_yr[0]
+    temp1 = df_slct.query(f'{year} == @sy').shape[0]
+
+delt = 100 * ( (temp0/temp1) - 1)
+
+reportcount = df_slct.shape[0]
+# display metrics about the data
+st.metric(label= 'Number of Incidents Reported', value= reportcount, delta= f'{str(int(delt))}%', delta_color= 'inverse')
+
+# Offense Category Pie Chart
+def show_ofns_cat(df_temp):
+    fig = px.pie(
+        df_temp,
+        names= category,
+        color_discrete_sequence= px.colors.sequential.RdBu
+    )
+
+    fig.update_traces(textposition= 'inside')
+    fig.update_layout(uniformtext_minsize = 12, uniformtext_mode= 'hide')
+
+    st.plotly_chart(fig)
+
+# Offense Description
+def show_ofns_desc(df_temp):
+
+    fig = px.pie(
+        df_temp.query('category in @slct_ofnsc'),
+        names= offense
+    )
+
+    fig.update_traces(textposition= 'inside')
+    fig.update_layout(uniformtext_minsize = 1212, uniformtext_mode= 'hide')
+
+    st.plotly_chart(fig)
 
 # option to see raw data
-c1, c2 = st.columns(2)
+show_sample = st.sidebar.button('Sample Data')
 
-with c1:
-    show_sample = st.sidebar.button('Sample Data')
-with c2:
-    extra = st.sidebar.button('extra')
+
 
 if show_sample:
     st.write(df_slct[['category', 'offense', 'borough', 'date']].sample(10))
+
+if not slct_ofnsc or len(slct_ofnsc) == 3:
+    show_ofns_cat(df_slct)
+else:
+    show_ofns_desc(df_slct)
 
 # def show_borough_line_chart(bor: list[str], years: list[int]):
 #     vcs = df_slct[['borough', 'year']].value_counts().reset_index(name= 'Count').sort_values(by= ['year'])
